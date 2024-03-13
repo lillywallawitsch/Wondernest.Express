8d10ad643f58a9a503ebfe089604dd0d96c860f9
lilly wallawitsch <lilly.wallawitsch@code.berlin>
2024-03-13 14:58:45 +0100

    changed app.listen text again

--- app.js
+++ app.js
@@ -58,5 +58,5 @@ const PORT = process.env.PORT || 3000;
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
